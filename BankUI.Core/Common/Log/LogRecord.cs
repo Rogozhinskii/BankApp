21fc1c7b5f2cd8b27ef939b4b8268c617bedbef6
Rogozhinskii <rogozhinskii96@mail.ru>
2021-12-15 08:36:33 +0500

    игнорировались два файла

---
+++ BankUI.Core/Common/Log/LogRecord.cs
@@ -0,0 +1,24 @@
+﻿namespace BankUI.Core.Common.Log
+{
+    /// <summary>
+    /// Запись в лог форме
+    /// </summary>
+    public class LogRecord
+    {
+        /// <summary>
+        /// сообщение
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Некий тип сообщения в логе
+        /// </summary>
+        public LogRecordLevel LogRecordLevel { get; set; }
+
+        public LogRecord()
+        {
+            LogRecordLevel = LogRecordLevel.None;
+            Message = string.Empty;
+        }
+    }
+}
---
+++ BankUI.Core/Common/Log/LogRecordLevel.cs
@@ -0,0 +1,13 @@
+﻿namespace BankUI.Core.Common.Log
+{
+    /// <summary>
+    /// Перечисление уровнений лога
+    /// </summary>
+    public enum LogRecordLevel
+    {
+        None=0,
+        Error=1,
+        Warning=2,
+        Info=3
+    }
+}
