be6889ae2abe04510c378b494d27e0bace2de7f9
Rogozhinskii <rogozhinskii96@mail.ru>
2021-12-17 23:05:12 +0500

    Игнорировались

---
+++ BankUI.Core/Common/Log/LogRecord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankUI.Core.Common.Log
+{
+    /// <summary>
+    /// Запись в лог форме
+    /// </summary>
+    public class LogRecord
+    {
+        /// <summary>
+        /// сообщение
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Некий тип сообщения в логе
+        /// </summary>
+        public LogRecordLevel LogRecordLevel { get; set; }
+
+        public LogRecord()
+        {
+            LogRecordLevel = LogRecordLevel.None;
+            Message = string.Empty;
+        }
+    }
+}
---
+++ BankUI.Core/Common/Log/LogRecordLevel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankUI.Core.Common.Log
+{
+    /// <summary>
+    /// Перечисление уровнений лога
+    /// </summary>
+    public enum LogRecordLevel
+    {
+        None = 0,
+        Error = 1,
+        Warning = 2,
+        Info = 3
+    }
+}
